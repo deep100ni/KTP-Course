af026fd8b8d3913573ddcce8e40fcff0ff4c9781
Deep100ni <deep100ni13@gmail.com>
2024-04-10 05:04:21 +0530

    Ep2 tasks

--- ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
+++ ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
@@ -1,9 +1,10 @@
 import com.faangx.ktp.basics.LeapYearCheckerMiniApp
 
 fun isLeapYear(year: Int): Boolean {
-    TODO()
+    // (divBy4 AND !divBy100) || divBy400
+    return (year % 4 == 0 && year % 100 != 0)||year % 400 ==0
 }
 
-fun LeapYearCheckerDemo() {
+fun main() {
     LeapYearCheckerMiniApp(::isLeapYear)
 }
