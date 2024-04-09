120512f2f42b67d8b6a6e640ce8909d4ae53dbce
Deep100ni <deep100ni13@gmail.com>
2024-04-10 05:04:21 +0530

    Ep2 tasks

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,13 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    return when{
+        age < 3 ->  "Infancy"
+        age < 12 -> "Childhood"
+        age < 18 -> "Teenage"
+        age < 60 -> "Adulthood"
+        else -> "Old age"
+    }
 }
 
 fun main() {
