db45658e1a6279d2fb800b37058fb8fa97ed0ee5
Vidhi Swarnkar <vidhiswarnkar@gmail.com>
2024-06-13 12:00:16 +0530

    Episode 3 completed

--- ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
@@ -1,11 +1,22 @@
 import com.faangx.ktp.basics.FactorCalculatorMiniApp
 
 fun printFactorsOf(num: Int) {
-    TODO()
+    for (i in 1..num){
+        if (num % i == 0){
+            print("$i, ")
+        }
+    }
 }
 
 fun isPrime(num: Int): Boolean {
-    TODO()
+    var factor = 0
+    for (i in 1..num){
+        if (num % i == 0){
+            factor ++
+        }
+    }
+    return factor ==2
+
 }
 
 fun main() {
