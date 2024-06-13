d94e5628b69803f81eb4ed092e79a6e7cccbf4d2
Vidhi Swarnkar <vidhiswarnkar@gmail.com>
2024-06-13 17:56:16 +0530

    Episode 3 ext 1 completed

--- ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
@@ -1,7 +1,16 @@
 import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines){i->
+        repeat(lines){j ->
+            if (j == lines - i - 1)
+                print(char1)
+            else
+                print(char2)
+
+        }
+        println()
+    }
 }
 
 fun main() {
