78b29a383c7203ad846e67d5c166b41e24dc5a14
deep100ni <deep100ni13@gmail.com>
2024-06-16 10:43:44 +0530

    update StringPalindromeChecker

--- ProgrammingFundamentals/Ep4/StringPalindrome/src/Task.kt
+++ ProgrammingFundamentals/Ep4/StringPalindrome/src/Task.kt
@@ -4,9 +4,8 @@ fun reverseStr(str: String): String {
 	return buildString {
 		for (i in str.lastIndex downTo 0) {
 			append(str[i])
-        }
-    }
-
+		}
+	}
 }
 
 fun isPalindrome(str: String, ignoreCase: Boolean): Boolean {
@@ -16,24 +15,23 @@ fun isPalindrome(str: String, ignoreCase: Boolean): Boolean {
 fun areStringsEqual(s1: String, s2: String, ignoreCase: Boolean): Boolean {
 	if (s1.length != s2.length) return false
 
-    for (i in s1.indices) {
+	for (i in s1.indices) {
 		if (!areCharsEqual(s1[i], s2[i], ignoreCase)) return false
-    }
-    return true
-
+	}
+	return true
 }
 
 fun areCharsEqual(c1: Char, c2: Char, ignoreCase: Boolean): Boolean {
-	 if (!ignoreCase) return c1 == c2
-     return convertToLowercase(c1) == convertToLowercase(c2)
+	if (!ignoreCase) return c1 == c2
+	return convertToLowercase(c1) == convertToLowercase(c2)
 }
 
 fun convertToLowercase(char: Char): Char {
 	return if (char in 'A'..'Z') {
 		Char(char.code + 32)
-    } else {
+	} else {
 		char
-    }
+	}
 }
 
 
