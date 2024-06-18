9324c3445cca9667903e64899e0684cb8d4be582
deep100ni <deep100ni13@gmail.com>
2024-06-18 14:05:02 +0530

    update StringCaseConvertor

--- ProgrammingFundamentals/Ep4/StringCaseConvertor/src/Task.kt
+++ ProgrammingFundamentals/Ep4/StringCaseConvertor/src/Task.kt
@@ -1,32 +1,85 @@
 import com.faangx.ktp.basics.StringCaseConvertorMiniApp
 
 fun convertToSentenceCase(text: String): String {
-    TODO()
+	var capitalize = true
+	var requireSpace = false
+	return buildString {
+		for (char in text) {
+			if (requireSpace) {
+				if (char != ' ') append(' ')
+				requireSpace = false
+			}
+			if (capitalize) {
+				append(convertToUppercase(char))
+				capitalize = false
+			} else {
+				append(char)
+			}
+			if (char in listOf('.', '!', '?')) {
+				capitalize = true
+				requireSpace = true
+			}
+		}
+	}
 }
 
 fun convertToTitleCase(text: String): String {
-    return "TODO()"
+	var capitalize = true
+	return buildString {
+		for (char in text) {
+			if (char != ' ') {
+				if (capitalize) {
+					append(convertToUppercase(char))
+					capitalize = false
+				} else {
+					append(char)
+				}
+			}
+			if (char in listOf(' ', '!', '?', '.', ":", ';', ',')) {
+				capitalize = true
+			}
+		}
+	}
 }
 
 fun convertToUppercase(text: String): String {
-    return "TODO()"
+	return convertToUppercase(text)
 }
 
 fun convertToLowercase(text: String): String {
-    return "TODO()"
+	return convertToLowercase(text)
 }
 
 fun convertToSnakeCase(text: String): String {
-    return "TODO()"
+	return buildString {
+		for (char in text) {
+			if (char == ' ') append('_')
+		}
+	}
 }
 
 fun convertToUppercase(char: Char): Char {
-    TODO()
+	return if (char in 'a'..'z') {
+		val upperChar = char - 32
+		(upperChar)
+	} else {
+		(char)
+	}
 }
 
 fun convertToLowercase(char: Char): Char {
-    TODO()
+	return if (char in 'A'..'Z') {
+		val lowerChar = char + 32
+		(lowerChar)
+	} else {
+		(char)
+	}
 }
+
+fun isAlphabet(char: Char): Boolean {
+	return char in 'a'..'z' || char in 'A'..'Z'
+}
+
 
 fun main() {
     StringCaseConvertorMiniApp(
